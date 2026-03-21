66fc838399c152f8950f969db2872b40824e5138
Carolina Yut <carolina.yut@mi.unc.edu.ar>
2026-03-21 16:35:01 -0300

    Ejercicio 1 completo

--- src/main/scala/FileIO.scala
+++ src/main/scala/FileIO.scala
@@ -1,17 +1,69 @@
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
 import scala.io.Source
 
+
 object FileIO {
-  // Pure function to read subscriptions from a JSON file
+  
+type Subscription = (String, String)
+implicit val formats: Formats = DefaultFormats 
+
   def readSubscriptions(): List[String] = {
-    List(
-      "https://www.reddit.com/r/scala/.json?count=10",
-      "https://www.reddit.com/r/learnprogramming/.json?count=10"
-    )
+    loadSubscriptions("subscriptions.json") match {
+      case Some(subscriptions) =>
+        subscriptions.map { case (_, url) => url }
+      case None =>
+        List()
+    }
   }
 
-  // Pure function to download JSON feed from a URL
   def downloadFeed(url: String): String = {
     val source = Source.fromURL(url)
-    source.mkString
+    val content = source.mkString
+    source.close()
+    content
   }
+
+  def readFile (path: String): Option[String] = {
+    try {
+      val source = Source.fromFile(path)
+      val content = source.mkString 
+      source.close()
+      Some(content)
+
+  }
+    catch {
+      case _: Exception => None
+  } 
 }
+
+  def parserSubscriptions (content:String): Option[List[Subscription]] = {
+    try {
+      val json = parse(content)
+      json match{
+        case JArray(arr) =>
+          Some(
+              arr.map { elemento =>
+              val name = (elemento \ "name").extract[String]
+              val url = (elemento \ "url").extract[String]
+              (name,url) 
+                      }
+              )
+        case _ => None
+                }
+         }
+    catch {
+      case _: Exception => None
+    }
+  }
+
+  def loadSubscriptions(path: String): Option[List[Subscription]] = {
+    readFile(path) match {
+      case Some(content) => parserSubscriptions(content)
+      case None          => None
+    }
+  } 
+
+
+}
+
