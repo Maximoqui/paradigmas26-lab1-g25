92103d0fd19bb8d34b6dac026c2458e7fb0aa000
Estudiante maxi <maximo.quinones@mi.unc.edu.ar>
2026-03-29 10:51:32 -0300

    feat: agregar estadísticas de palabras y top 5 posts al informe

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,35 +3,53 @@ object Main {
 
     val header = s"Reddit Post Parser\n${"=" * 40}"
 
-    // 1. Leer URLs de subscriptions.json
-    val subscriptions: List[String] = FileIO.readSubscriptions()
+    // 1. Leer name + url
+    val subscriptions: List[FileIO.Subscription] = FileIO.loadSubscriptions("subscriptions.json").getOrElse(List())
 
-    // 2. Descargar y parsear los posts de cada URL
-    val allPosts: List[(String, List[FileIO.Post])] = subscriptions.map { url =>
+    // 2. Descargar posts
+    val allPosts = subscriptions.map { case (name, url) =>
       println(s"Fetching posts from: $url")
 
-      val posts: List[FileIO.Post] = FileIO.downloadFeed(url)
+      val posts = FileIO.downloadFeed(url)
         .map(FileIO.postList)
         .getOrElse(List())
 
-      (url, posts)
+      (name, url, posts)
     }
 
-    // 3. Generar el output final
-    val output = allPosts.map { case (url, posts) =>
+    // 3. Output completo
+    val output = allPosts.map { case (name, url, posts) =>
 
-      // calcular score total
       val totalScore = FileIO.totalScore(posts)
 
+      // palabras más frecuentes
+      val allText = posts.map(_._3).mkString(" ")
+      val palabras = FileIO.countwords(allText)
+        .toList
+        .sortBy(-_._2)
+        .take(5)
+
+      // top 5 posts
+      val top5 = posts
+        .sortBy { case (_, _, _, _, score) => -score }
+        .take(5)
+
       s"""
+Subreddit: $name
 URL: $url
 Total Score: $totalScore
 Cantidad de posts: ${posts.length}
-"""
-    }
-    .mkString("\n")
 
-    // 4. Imprimir resultado
+Top palabras:
+${palabras.map { case (p, c) => s"$p: $c" }.mkString("\n")}
+
+Top 5 posts:
+${top5.map { case (_, titulo, _, fecha, score) =>
+  s"$titulo | $fecha | Score: $score"
+}.mkString("\n")}
+"""
+    }.mkString("\n============================\n")
+
     println(header)
     println(output)
   }
